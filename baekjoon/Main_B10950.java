1525bfccff65f6063c88df7744aeda0edc0ae25f
Kihoon-Park <arlecchino94@naver.com>
2020-01-28 17:50:12 +0900

    20/01/28 algorithm - Baekjoon

---
+++ baekjoon/Main_B10950.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main_B10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+	}
+
+}
---
+++ baekjoon/Main_B8393.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_B8393 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int i = sc.nextInt();
+
+		System.out.println(i * (i + 1) / 2);
+	}
+
+}
